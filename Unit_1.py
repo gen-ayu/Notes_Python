e8aa3d356042efeaf87b0665ef6c226ed373a2ad
gen-ayu <ayush.anand.giri@gmail.com>
2026-06-12 20:24:45 +0530

    Updated Unit 1 has been shared

--- Unit_1.py
+++ Unit_1.py
@@ -1,5 +1,51 @@
-print("Hello World")
-if(5 > 3 and 5 < 10):
-    print("5 is greater than 3 and less than 10")
-elif (5 > 3 or 5 < 10):
-    print("5 is greater than 3 or less than 10")
+# We will be beginning to learn about python in this unit.
+# We will bw starting from the basic concepts of python and then we will be moving on to the advanced concepts of python.
+
+'''-------------------------------------------------lets begin---------------------------------------------------'''
+
+'''Starting off with comments in python
+Comments are of two types in pyhton
+sinlge line comments - use # to create single line comments
+multi line comments - use triple single quotes or triple double quotes to create multi line comments'''
+
+# White space in python
+'''In python, white space is used to indicate the end of a statement.
+Tab and space are used to create white space in python. It essintally works as an indentifier for the end of a statement just like parenthisis of curly braces do in other languages.'''
+
+# Identifiers and keywords in python
+'''Identifiers are the names given to variables, functions, classes, etc. in python. They are used to identify the different elements in a python program. 
+For example, if we have a variable named 'x' and a function named 'my_function', then 'x' and 'my_function' are identifiers in python.  
+Keywords are the reserved words in python that have a special meaning and cannot be used as identifiers.
+For example, if, else, while, for, etc. are keywords in python.'''
+
+# Moving on to variables and assignment statements in python
+'''Variables are used to store data in python. They are created by assigning a value to a name. For example, if we want to create a variable named 'x' and assign it the value of 10, we can do it like this: x = 10.
+In this example, 'x' is the variable name and '10' is the value assigned to the variable. We can also assign the value of one variable to another variable. 
+For example, if we want to assign the value of 'x' to a new variable named 'y', we can do it like this: y = x. In this example, 'y' will now have the value of 10, which is the value of 'x'.'''
+''' we can also do chained assignment in python. For example, if we want to assign the value of 10 to both 'x' and 'y', we can do it like this: x = y = 10. In this example, both 'x' and 'y' will have the value of 10.'''
+
+# We will now be learning about the different data types in python.
+'''In python, there are several built-in data types that we can use to store different types of data. Some of the most commonly used data types in python are:
+1. int - used to store integer values (whole numbers) represented without a decimal point
+2. float - used to store floating-point values (decimal numbers) represented with a decimal point
+3. str - used to store string values (text) represented with single quotes, double quotes, or triple quotes
+4. bool - used to store boolean values (True or False) represented with the keywords True and False
+5. list - used to store a collection of values (ordered and mutable) represented with square brackets []
+6. tuple - used to store a collection of values (ordered and immutable) represented with parentheses ()
+7. dict - used to store a collection of key-value pairs (unordered and mutable) represented with curly braces {} 
+8. set - used to store a collection of unique values (unordered and mutable) represented with curly braces {}'''
+
+''' The difference btw a set and a tuple is that a set is mutable and does not allow duplicate values, while a tuple is immutable and allows duplicate values.
+The difference btw a list and a tuple is that a list is mutable and allows duplicate values, while a tuple is immutable and allows duplicate values.'''
+
+# We will now be learning about the different operators in python.
+'''In python, there are several types of operators that we can use to perform different operations on our data. Some of the most commonly used operators in python are:
+1. Arithmetic operators - used to perform mathematical operations on numbers. For example, +, -, *, /, %, **, //
+2. Comparison operators - used to compare two values and return a boolean result. For example, ==, !=, >, <, >=, <=
+3. Logical operators - used to combine multiple boolean expressions and return a boolean result. For example, and, or, not
+4. Assignment operators - used to assign a value to a variable. For example, =, +=, -=, *=, /=, %=, **=, //=
+5. Bitwise operators - used to perform bitwise operations on integers. For example, &, |, ^, ~, <<, >> (by name these bitwise operator are called as and, or, xor, not, left shift, right shift respectively)
+6. Identity operators - used to compare the memory location of two objects. For example, is, is not
+7. Membership operators - used to check if a value is present in a sequence (like list, tuple, string). For example, in, not in'''
+
+# These are a few of the basic concepts of python that we will be learning in this unit. We will be covering more advanced concepts in the upcoming units.git 
