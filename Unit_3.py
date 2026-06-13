119121180db3712b5e403ab6b1c72e99b466ca8b
gen-ayu <ayush.anand.giri@gmail.com>
2026-06-13 22:53:57 +0530

    Uploading miscellaneous changes

---
+++ Unit_3.py
@@ -0,0 +1,8 @@
+# Let's start with the unit 3 of our python course. In this unit we will learn about functions, modules, packages, recursion.
+
+# Functions are reusable blocks of code that perform a specific task. They let you break a program into smaller, easier-to-manage pieces. By calling a function whenever you need its behavior, you avoid repeating code and make your program easier to read and maintain. The syntax for defining a function in Python is:
+
+''' def function_name(parameters):
+        """Optional docstring describing what the function does."""
+        # function body
+        return value # optional return statement '''
