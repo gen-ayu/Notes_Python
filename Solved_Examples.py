4ea160cdd9f563f7631af0dc510f769b54db2dfa
gen-ayu <ayush.anand.giri@gmail.com>
2026-06-13 21:40:06 +0530

    Uploading Examples/ Short notes for Unit 1

---
+++ Solved_Examples.py
@@ -0,0 +1,15 @@
+'''----------------------------------------UNIT 1: INTRODUCTION TO PYTHON----------------------------------------'''
+
+# Examples related to the topics covered in Unit 1 are provided below. 
+# These examples will help you understand the concepts better and give you a chance to practice what you have learned.
+
+''' Well it sucks but the topics we covered in unit 1 are mostly theoretical and there are not many examples that we can provide for those topics. 
+So I will just try to summarize the topics real quick and move to the next unit where we can actually do some fun stuff.
+
+Unit 1 covers basic theory and concepts of Python:
+Comments (#, double and single quotes)
+White space and indentation
+Identifiers and keywords
+Variables and assignment (including chained assignment)
+Data types: int, float, str, bool, list, tuple, dict, set
+Operators: arithmetic, comparison, logical, assignment, bitwise, identity, membership '''
